a0b287b64acc27fa0f4584712dbf56b2351d593d
xdeveloper <alexander.abakumov@gmail.com>
2016-11-02 13:35:38 +0200

    New class instead of deprecated one

--- src/main/java/ua/com/abakumov/doit/Application.java
+++ src/main/java/ua/com/abakumov/doit/Application.java
@@ -3,7 +3,7 @@ package ua.com.abakumov.doit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
